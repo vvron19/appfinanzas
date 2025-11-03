ada824ffcf05cf0b88b98dcaf2b029b6e81615f8
vvron19 <aaroncuriqueo821@gmail.com>
2025-11-03 04:36:35 -0300

    Añadir config.js para la publicación en GitHub Pages

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB2t6IQKCUEwFiRXM7QzQHbOyR1RzAXSHU",
+  authDomain: "app-de-finanzas-estudiantil.firebaseapp.com",
+  projectId: "app-de-finanzas-estudiantil",
+  storageBucket: "app-de-finanzas-estudiantil.firebaseapp.com",
+  messagingSenderId: "161010697405",
+  appId: "1:161010697405:web:90e8b839b1f91a1f81e3d4",
+  measurementId: "G-WWEE415H5Q"
+};
