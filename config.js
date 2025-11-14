7824bc0a21d7af95ee42e8f305dce881299a3359
vvron19 <aaroncuriqueo821@gmail.com>
2025-11-13 22:55:38 -0300

    Actualización del overlay del login

---
+++ config.js
@@ -0,0 +1,13 @@
+// Para Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB2t6IQKCUEwFiRXM7QzQHbOyR1RzAXSHU",
+    authDomain: "app-de-finanzas-estudiantil.firebaseapp.com",
+    projectId: "app-de-finanzas-estudiantil",
+    storageBucket: "app-de-finanzas-estudiantil.firebasestorage.app",
+    messagingSenderId: "161010697405",
+    appId: "1:161010697405:web:90e8b839b1f91a1f81e3d4",
+    measurementId: "G-WWEE415H5Q"
+};
+
+// ESTA LÍNEA ES CRUCIAL para solucionar los errores de importación (Uncaught SyntaxError)
+export default firebaseConfig;
